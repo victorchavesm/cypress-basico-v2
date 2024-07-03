a4caa47bbcfa752e901ad928e11035167d00d5fd
Victor Chaves <104216761+victorchavesm@users.noreply.github.com>
2024-07-03 19:19:36 -0300

    teste1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,8 +9,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatorios e envia o formulario', function(){
+        cy.get('#firstName').type('Victor');
+        cy.get('#lastName').type('Chaves');
+        cy.get('#email').type('victor@gmail.com');
+        cy.get('#open-text-area').type('Lorem ipsum');
+        cy.get('button[type="submit"]').click();
+        cy.get('.success').should('be.visible');
+        
     })
   })
